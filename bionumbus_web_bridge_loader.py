cbae6090a65147398dc589a88c869ce01598f85f
migbro <miguel.a.brown@gmail.com>
2016-05-26 15:12:59 -0500

    Added helper renaming script

--- bionumbus_web_bridge_loader.py
+++ bionumbus_web_bridge_loader.py
@@ -14,16 +14,17 @@ def date_time():
     return cur
 
 
-def get_source_list(user, server, directory, log):
+def get_source_list(user, server, directory, log, machine):
     rsync_cmd = 'rsync --list-only -r ' + user + '@' + server + ':' + directory
     try:
-        seqfiles = []
+        # switching to hash model - will have input file name, output file name in case input isn't formatted properly
+        seqfiles = {}
         flist = subprocess.check_output(rsync_cmd, shell=True)
         for item in flist.split('\n'):
-            if re.search('\S+\.sequence\.txt\.gz$', item):
+            if re.search('\S+_sequence\.txt\.gz$', item) is not None:
                 # split on white space just to get dir/file
                 info = item.split()
-                seqfiles.append(info[-1])
+                seqfiles[info[-1]] = info[-1]
         return seqfiles
 
     except:
@@ -32,46 +33,44 @@ def get_source_list(user, server, directory, log):
         exit(1)
 
 
-def search_and_desync(sf, dest, log, user, server, directory, new_dir_list):
-    cur = dest + sf
-    if not os.path.isfile(cur):
-        log.write(date_time() + 'Syncing file ' + sf + '\n')
-        log.flush()
-        # check to see if directory exists first!
-        check_dir = dest + os.path.dirname(sf)
-        if not os.path.isdir(check_dir):
-            os.mkdir(check_dir, mode=0o755)
-            new_dir_list.append(check_dir)
-            log.write('Made dir ' + check_dir + '\n')
-        rsync_cmd = 'rsync -rtv ' + user + '@' + server + ':' + directory + sf + ' ' + dest + sf
-        check = subprocess.call(rsync_cmd, shell=True)
-        if check == 0:
-            log.write(date_time() + ' File transfer successful!\n')
+def search_and_desync(seqdict, dest, log, user, server, directory, new_dir_list):
+    for sf in seqdict:
+        cur = dest + sf
+        if not os.path.isfile(cur):
+            log.write(date_time() + 'Syncing file ' + sf + '\n')
             log.flush()
-            return new_dir_list
-        else:
-            log.write(date_time() + 'File transfer failed using command: ' + rsync_cmd + ' FIX IT\n')
-            log.flush()
-            exit(1)
+            # check to see if directory exists first!
+            check_dir = dest + os.path.dirname(seqdict[sf])
+            if not os.path.isdir(check_dir):
+                os.mkdir(check_dir, mode=0o755)
+                new_dir_list.append(check_dir)
+                log.write('Made dir ' + check_dir + '\n')
+            rsync_cmd = 'rsync -rtvL ' + user + '@' + server + ':' + directory + sf + ' ' + dest + seqdict[sf]
+            check = subprocess.call(rsync_cmd, shell=True)
+            if check != 0:
+                log.write(date_time() + 'File transfer failed using command: ' + rsync_cmd + ' FIX IT\n')
+                log.flush()
+                exit(1)
+            # move file to COPIED folder so that it's not moved again
+
+    return new_dir_list
 
 
 def synergize(config_file):
     config_data = json.loads(open(config_file, 'r').read())
-    (source_user, source_server, source_dir, dest_dir, log_dir) = (config_data['source-user'], config_data['source-ip'],
-    config_data['source-dir'], config_data['destination-dir'], config_data['log-dir'])
+    (source_user, source_server, source_dir, dest_dir, log_dir, machine) = (config_data['source-user'], config_data['source-ip'],
+    config_data['source-dir'], config_data['destination-dir'], config_data['log-dir'], config_data['machine'])
     log = open(log_dir + config_file[:-5], 'a')
     log.write(date_time() + 'Getting source file list\n')
-    source_list = get_source_list(source_user, source_server, source_dir, log)
-    if len(source_list) < 1:
+    source_dict = get_source_list(source_user, source_server, source_dir, log, machine)
+    if len(source_dict) < 1:
         log.write('Sequencing files found...eject!\n')
         log.flush()
         return 0
     else:
-        new_dir_list = []
+
         log.write('Searching destination for found sequencing files\n')
-        for seqfile in source_list:
-            new_dir_list = search_and_desync(seqfile, dest_dir, log, source_user, source_server, source_dir,
-                                             new_dir_list)
+        new_dir_list = search_and_desync(source_dict, dest_dir, log, source_user, source_server, source_dir)
     for dirs in new_dir_list:
         final_file_cmd = 'touch ' + dirs + '/import.me ' + dirs + '/sync.me'
         subprocess.call(final_file_cmd)
---
+++ fudge_it.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import argparse
+import os
+import sys
+import re
+import subprocess
+import time
+
+
+def date_as_int():
+    return time.strftime("%y%m%d")
+
+
+def fudge_it(dirname, machine):
+    # helper function to create symbolic link with correctly named file
+    # see if it matches this format: 2016_337_GTGAAAC_160512_D00422_0315_AHMLHHBCXX_L002_R1_001.fastq.gz
+    find_cmd = 'find ' + dirname + ' -name \'*.fastq.gz\''
+    flist = subprocess.check_output(find_cmd, shell=True)
+    for fn in flist.split('\n'):
+        test = re.search('(\d+-|_\d+)_\w+_(\d+_\w+_\d+_\w+)_L00(\d)_R(\d)_\d+\.fastq\.gz', fn)
+        try:
+            (bid, run, lane, end) = (test.group(1), test.group(2), test.group(3), test.group(4))
+            if not os.path.isdir(run):
+                os.mkdir(run, 0o755)
+            symlink = run + '/' + '_'.join((bid, run, lane, end)) + '_sequence.txt.gz'
+            if not os.path.isfile(symlink):
+                mklink = 'ln -s fn ' + symlink
+                subprocess.call(mklink, shell=True)
+
+        except:
+            # 2016-1019_ATCACGA_L002_R2_001.fastq.gz
+            sys.stderr.write('First format failed.  Trying second format\n')
+            test = re.search('(\d+-|_\d+)_\w+_L00(\d)_R(\d)_\d+\.fastq\.gz', fn)
+            try:
+                (bid, lane, end) = (test.group(1), test.group(2), test.group(3))
+                date_str = str(date_as_int())
+                run = '_'.join((date_str, machine, '0000', 'AXXXXXXXXX'))
+                if not os.path.isdir(run):
+                    os.mkdir(run, 0o755)
+                symlink = run + '/' + '_'.join((bid, run, lane, end)) + '_sequence.txt.gz'
+                if not os.path.isfile(symlink):
+                    mklink = 'ln -s fn ' + symlink
+                    subprocess.call(mklink, shell=True)
+            except:
+                sys.stderr.write('Could not reformat ' + fn + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Searches for sequence files that aren\'t "HGAC compatible" and creates symlinks correctly'
+                    ' formatted')
+    parser.add_argument('-d', '--dir', action='store', dest='dirname', help='directory to search')
+    parser.add_argument('-m', '--machine', action='store', dest='machine', help='machine name to use to fudge')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
+    fudge_it(args.dirname, args.machine)
+
+if __name__ == '__main__':
+    main()
