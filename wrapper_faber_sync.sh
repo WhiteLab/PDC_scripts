7c6d3d96c2d77ad418e77e896068bacd044dbb01
migbro <miguel.a.brown@gmail.com>
2016-07-08 14:25:09 -0500

    Corrected a missing underscore in file name

--- wrapper_faber_sync.sh
+++ wrapper_faber_sync.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# This is a wrapper script to be called by cron to initiate a bionimbus web bridge transfer to the bionimbus web site. This is being wrapped to help create a more dynamic method to # be able to change the log file destination.
+# This is a wrapper script to be called by cron to initiate a bionimbus web bridge transfer to the bionimbus web site.
+# This is being wrapped to help create a more dynamic method to # be able to change the log file destination.
 
 NOW=$(date +"%y%m%d")
 cd /home/mbrown/core_xfer_configs && ../PDC_scripts/bionumbus_web_bridge_loader.py -j faber_core.json 2>> logs/$NOW'_faber_sync.log' >> logs/$NOW'_faber_sync.log' &
