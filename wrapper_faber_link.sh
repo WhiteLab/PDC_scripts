9839ae360dc5a0c038e1864b5687fc5018073b7f
Miguel Brown account <mbrown@beaker.uchicago.edu>
2016-07-08 11:44:32 -0500

    Added wrapper scripts for symlink creation

---
+++ wrapper_faber_link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# This is a wrapper script to be called by cron to generate symlinks for files transfered by partner cores. This is being wrapped to help create a more dynamic method to # be able to change the log file destination.
+
+NOW=$(date +"%y%m%d")
+cd /filestore/LOGS && ../PDC_scripts/fudge_it_PF.py -d /filestore/SFTP_SHARE/pfaber/pfaber_data -m FLOWCELLID 2>> $NOW'_faber_link.log' >> $NOW'_faber_link.log' &
