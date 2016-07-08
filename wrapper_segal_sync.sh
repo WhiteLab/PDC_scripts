bb33422976ffd1a8afb8a1ee9eae3c342eed26c0
migbro <mbrown@bc.bionimbus.org>
2016-07-08 16:19:13 -0500

    Log file name fix

--- wrapper_segal_sync.sh
+++ wrapper_segal_sync.sh
@@ -4,4 +4,4 @@
 # This is being wrapped to help create a more dynamic method to # be able to change the log file destination.
 
 NOW=$(date +"%y%m%d")
-cd /home/mbrown/core_xfer_configs && ../PDC_scripts/bionumbus_web_bridge_loader.py -j segal_core.json 2>> logs/$NOW'_segal_sync.log' >> logs/$NOW'_segal.log' &
+cd /home/mbrown/core_xfer_configs && ../PDC_scripts/bionumbus_web_bridge_loader.py -j segal_core.json 2>> logs/$NOW'_segal_sync.log' >> logs/$NOW'_segal_sync.log' &
