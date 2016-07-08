d68d2838707359b86158265d27830ac5f890b2b7
migbro <mbrown@bc.bionimbus.org>
2016-07-08 11:32:01 -0500

    Adding Luke's wrapper scripts for cronjobs

---
+++ wrapper_segal_sync.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# This is a wrapper script to be called by cron to initiate a bionimbus web bridge transfer to the bionimbus web site. This is being wrapped to help create a more dynamic method to # be able to change the log file destination.
+
+NOW=$(date +"%y%m%d")
+cd /home/mbrown/core_xfer_configs && ../PDC_scripts/bionumbus_web_bridge_loader.py -j segal_core.json 2>> logs/$NOW'segal_sync.log' >> logs/$NOW'_segal.log' &
