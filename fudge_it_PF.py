0e1b0ec7380b0941f0fb9df6b8e2c4bc3449fb15
migbro <miguel.a.brown@gmail.com>
2016-08-06 19:17:52 -0500

    Added some flexibility in looking for _ or - in some spots.

--- fudge_it_PF.py
+++ fudge_it_PF.py
@@ -43,7 +43,7 @@ def fudge_it_pf(dirname, machine, links):
         except:
             # 2016-1019_ATCACGA_L002_R2_001.fastq.gz
             sys.stderr.write('First format failed.  Trying second format\n')
-            test = re.search('\w+[-|_](\d+-\d+)[-|_](\d{6}_\w+_\d+_\w{10})_S\d+_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
+            test = re.search('\w+[-|_](\d+-\d+)[-|_](\d{6}[_|]\w+[_|-]\d+[_|-]\w{10})_S\d+_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
             try:
                 (bid, run, lane, end) = (test.group(1), test.group(2), test.group(3), test.group(4))
                 sys.stderr.write('trying regex 2 ok for ' + fn + ' making link\n')
