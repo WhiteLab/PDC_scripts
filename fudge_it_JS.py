b16b094c5bdf7d518b0c2e7022e910413162a9a2
migbro <miguel.a.brown@gmail.com>
2017-07-31 16:25:30 -0500

    Updated segal transfer naming convention - software changes necessitate this

--- fudge_it_JS.py
+++ fudge_it_JS.py
@@ -13,12 +13,12 @@ def date_as_int():
 
 def fudge_it(dirname, machine):
     # helper function to create symbolic link with correctly named file
-    # see if it matches this format: 2016-337_GTGAAAC_160512_D00422_0315_AHMLHHBCXX_L002_R1_001.fastq.gz
+    # see if it matches this format: 2017-540_GACAGTG_merged_170707_D00422_0554_AHMHL5BCXY_R1_001.fastq.gz2016-337_GTGAAAC_160512_D00422_0315_AHMLHHBCXX_L002_R1_001.fastq.gz
     find_cmd = 'find ' + dirname + ' -name \'*.fastq.gz\''
     flist = subprocess.check_output(find_cmd, shell=True)
     flist = flist.rstrip('\n')
     for fn in flist.split('\n'):
-        test = re.search('(\d{4}[-_]\d+)_\D+_(\d{6}_\w+_\d+_\w{10})_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
+        test = re.search('(\d{4}[-_]\d+)_\D+_merged_(\d{6}_\w+_\d+_\w{10})_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
         try:
             (bid, run, lane, end) = (test.group(1), test.group(2), test.group(3), test.group(4))
             sys.stderr.write('regex 1 ok for ' + fn + ' checking for existing link\n')
@@ -33,26 +33,23 @@ def fudge_it(dirname, machine):
                 sys.stderr.write('Creating link for ' + fn + ' ' + mklink + '\n')
 
         except:
-            # 2016-1019_ATCACGA_L002_R2_001.fastq.gz
-            sys.stderr.write('Could not reformat ' + fn + '\n')
-            # sys.stderr.write('First format failed.  Trying second format\n')
-            # test = re.search('(\d+[-_]\d+)_\w+_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
-            # try:
-            #     (bid, lane, end) = (test.group(1), test.group(2), test.group(3))
-            #     sys.stderr.write('regex 2 ok for ' + fn + ' checking for existing link\n')
-            #     bid = bid.replace('_', '-')
-            #     date_str = str(date_as_int())
-            #     run = '_'.join((date_str, machine, '0000', 'AXXXXXXXXX'))
-            #     run_path = dirname + '/' + run
-            #     if not os.path.isdir(run_path):
-            #         os.mkdir(run_path, 0o755)
-            #     symlink = run_path + '/' + '_'.join((bid, run, lane, end)) + '_sequence.txt.gz'
-            #     if not os.path.isfile(symlink):
-            #         mklink = 'ln -s ' + fn + ' ' + symlink
-            #         subprocess.call(mklink, shell=True)
-            #         sys.stderr.write('Creating link for ' + fn + ' ' + mklink + '\n')
-            # except:
-            #     sys.stderr.write('Could not reformat ' + fn + '\n')
+            # 2016-337_GTGAAAC_160512_D00422_0315_AHMLHHBCXX_L002_R1_001.fastq.gz
+            sys.stderr.write('First format failed.  Trying second format\n')
+            test = re.search('(\d{4}[-_]\d+)_\D+_(\d{6}_\w+_\d+_\w{10})_L00(\d)_R(\d)_\d+\.fastq\.gz$', fn)
+            try:
+                (bid, run, lane, end) = (test.group(1), test.group(2), test.group(3), test.group(4))
+                sys.stderr.write('regex 1 ok for ' + fn + ' checking for existing link\n')
+                bid = bid.replace('_', '-')
+                run_path = dirname + '/' + run
+                if not os.path.isdir(run_path):
+                    os.mkdir(run_path, 0o755)
+                symlink = run_path + '/' + '_'.join((bid, run, lane, end)) + '_sequence.txt.gz'
+                if not os.path.isfile(symlink):
+                    mklink = 'ln -s ' + fn + ' ' + symlink
+                    subprocess.call(mklink, shell=True)
+                    sys.stderr.write('Creating link for ' + fn + ' ' + mklink + '\n')
+            except:
+                sys.stderr.write('Could not reformat ' + fn + '\n')
 
 
 def main():
